d15c160df0c6b7aa5900e68ea73cbbaa4ae48d6b
wheat-thin-wiens <ethanjwiens@gmail.com>
2025-02-11 20:18:06 -0600

    add config.lua

---
+++ lua/rei/config.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.defaults = {
+  terminal_colors = true,
+  styles = {
+    comments = {},
+    keywords = {},
+    identifiers = {},
+    functions = {},
+    variables = {},
+    booleans = {}
+  },
+  integrations = {},
+  highlight_overrides = {}
+}
+
+return M
--- lua/rei/init.lua
+++ lua/rei/init.lua
@@ -1,9 +1,26 @@
 local M = {}
+local config = require("rei.config")
 
-M.defaults = {}
+M.colorscheme = function()
+  vim.cmd("hi clear")
+  if vim.fn.exists("syntax_on") then
+    vim.cmd("syntax reset")
+  end
+
+  vim.o.background = "dark"
+  vim.o.termguicolors = true
+  vim.g.colorsname = "rei"
+end
 
 M.setup = function(opts)
-  opts = opts or M.defaults
+  opts = opts or config.defaults
+
+  for k, v in pairs(opts) do
+    if k == "integrations" then
+      for int, en in pairs(v) do
+      end
+    end
+  end
 end
 
 return M
