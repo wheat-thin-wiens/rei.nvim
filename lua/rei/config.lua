fcfd4b8c4b75f9dd1a7f0313c3327324ddebf182
wheat-thin-wiens <ethanjwiens@gmail.com>
2025-02-11 22:07:26 -0600

    finish config.setup

--- lua/rei/config.lua
+++ lua/rei/config.lua
@@ -1,6 +1,7 @@
 local M = {}
 
-M.defaults = {
+-- M.defaults = {
+local defaults = {
   terminal_colors = true,
   styles = {
     comments = {},
@@ -18,6 +19,22 @@ M.defaults = {
 
 M.setup = function(opts)
   opts = opts or M.defaults
+
+  for k, v in pairs(opts) do
+    if k == "integrations" then
+      for integration, enabled in pairs(v) do
+        defaults.integrations[integration] = enabled
+      end
+    elseif k == "styles" then
+      for style_k, style_v in pairs(v) do
+        if defaults.styles[style_k] ~= nil then
+          M.styles[style_k] = vim.tbl_deep_extend("keep", defaults.styles[style_k], style_v)
+        end
+      end
+    else
+      defaults[k] = v
+    end
+  end
 end
 
-return setmetatable(M, { __index = M.defaults })
+return setmetatable(M, { __index = defaults })
