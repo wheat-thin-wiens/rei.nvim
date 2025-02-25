b81d0a4ca703a18878c75b0c42a83e44ab3b0991
wheat-thin-wiens <ethanjwiens@gmail.com>
2025-02-25 15:59:42 -0600

    palette refactor

--- lua/rei/groups/gitsigns.lua
+++ lua/rei/groups/gitsigns.lua
@@ -1,8 +1,6 @@
-local p = require("rei.palette").defaults
-
 return {
   SignColumn = { fg = "None", bg = "None" },
-  GitSignsAdd    = { fg = p.green, bg = "None" },
-  GitSignsChange = { fg = p.yellow, bg = "None" },
-  GitSignsDelete = { fg = p.red, bg = "None" }
+  GitSignsAdd    = { fg = P.green, bg = "None" },
+  GitSignsChange = { fg = P.yellow, bg = "None" },
+  GitSignsDelete = { fg = P.red, bg = "None" }
 }
--- lua/rei/groups/telescope.lua
+++ lua/rei/groups/telescope.lua
@@ -1,44 +1,42 @@
 local M = {}
 
-local p = require("rei.palette").defaults
-
 M.default = {
-  TelescopeNormal = { link = "Normal" },
-  TelescopeBorder = { fg = p.white, bg = p.bg },
+  TelescopeNormal        = { link = "Normal" },
+  TelescopeBorder        = { fg = P.white, bg = P.bg },
 
-  TelescopePromptNormal = { bg = p.bg_dark },
-  TelescopePromptTitle = { fg = p.purple },
-  TelescopePromptBorder = { fg = p.white, bg = p.bg_dark },
-  TelescopePromptPrefix = { fg = p.purple },
-  TelescopePromptCounter = { fg = p.purple },
+  TelescopePromptNormal  = { bg = P.bg_dark },
+  TelescopePromptTitle   = { fg = P.purple },
+  TelescopePromptBorder  = { fg = P.white, bg = P.bg_dark },
+  TelescopePromptPrefix  = { fg = P.purple },
+  TelescopePromptCounter = { fg = P.purple },
 
   TelescopePreviewNormal = { link = "TelescopePromptNormal" },
   TelescopePreviewBorder = { link = "TelescopePromptBorder" },
-  TelescopePreviewTitle = { link = "TelescopePromptTitle" },
+  TelescopePreviewTitle  = { link = "TelescopePromptTitle" },
 
   TelescopeResultsNormal = { link = "TelescopePromptNormal" },
   TelescopeResultsBorder = { link = "TelescopePromptBorder" },
-  TelescopeResultsTitle = { link = "TelescopePromptTitle" },
+  TelescopeResultsTitle  = { link = "TelescopePromptTitle" },
 }
 
 M.borderless =  {
-  TelescopeNormal = { link = "Normal" },
-  TelescopeBorder = { fg = p.white, bg = p.bg },
+  TelescopeNormal        = { link = "Normal" },
+  TelescopeBorder        = { fg = P.white, bg = P.bg },
 
-  TelescopePromptNormal = { bg = p.grey0 },
-  TelescopePromptTitle = { fg = p.purple, bg = p.dark_purple },
-  TelescopePromptBorder = { fg = p.grey0, bg = p.grey0 },
-  TelescopePromptPrefix = { fg = p.purple },
-  TelescopePromptCounter = { fg = p.purple },
+  TelescopePromptNormal  = { bg = P.grey0 },
+  TelescopePromptTitle   = { fg = P.purple, bg = P.dark_purple },
+  TelescopePromptBorder  = { fg = P.grey0, bg = P.grey0 },
+  TelescopePromptPrefix  = { fg = P.purple },
+  TelescopePromptCounter = { fg = P.purple },
 
-  TelescopePreviewNormal = { bg = p.bg },
-  TelescopePreviewBorder = { fg = p.bg, bg = p.bg },
-  TelescopePreviewTitle = { fg = p.blue, bg = p.dark_blue },
+  TelescopePreviewNormal = { bg = P.bg },
+  TelescopePreviewBorder = { fg = P.bg, bg = P.bg },
+  TelescopePreviewTitle  = { fg = P.blue, bg = P.dark_blue },
 
-  TelescopeResultsNormal = { bg = p.bg },
-  TelescopeResultsBorder = { fg = p.bg, bg = p.bg },
-  TelescopeResultsTitle = { fg = p.green, bg = p.dark_green },
-  -- TelescopeResultsTitle = { fg = p.bg },
+  TelescopeResultsNormal = { bg = P.bg },
+  TelescopeResultsBorder = { fg = P.bg, bg = P.bg },
+  TelescopeResultsTitle  = { fg = P.green, bg = P.dark_green },
+  -- TelescopeResultsTitle = { fg = P.bg },
 }
 
 return M
