a5f1d7d36df65aee5f802f0be8a7c40487416168
wheat-thin-wiens <ethanjwiens@gmail.com>
2025-03-19 10:26:26 -0500

    fix: unfixed visual line highlighting

--- colors/rei.lua
+++ colors/rei.lua
@@ -5,14 +5,14 @@ require("rei").colorscheme()
 
 local scheme = vim.api.nvim_exec2("colorscheme", { output = true })
 
-if vim.deep_equal(scheme, "rei") then
-  vim.api.nvim_create_autocmd("FocusGained", {
-    pattern = "*",
-    command = "hi! link Visual VisualActive"
-  })
-
-  vim.api.nvim_create_autocmd("FocusLost", {
-    pattern = "*",
-    command = "hi! link Visual VisualInactive"
-  })
-end
+-- if vim.deep_equal(scheme, "rei") then
+--   vim.api.nvim_create_autocmd("FocusGained", {
+--     pattern = "*",
+--     command = "hi! link Visual VisualActive"
+--   })
+--
+--   vim.api.nvim_create_autocmd("FocusLost", {
+--     pattern = "*",
+--     command = "hi! link Visual VisualInactive"
+--   })
+-- end
