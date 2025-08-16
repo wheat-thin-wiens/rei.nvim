ab0f9bd8a04115a18e9f31939f33a31e40b87743
wheat-thin-wiens <ethanjwiens@gmail.com>
2025-08-15 22:48:02 -0500

    feat: start ts support

--- lua/rei/lang/init.lua
+++ lua/rei/lang/init.lua
@@ -4,7 +4,8 @@ P = require("rei.palette").defaults
 Transparency = require("rei.utils").transparency_enabled()
 
 M.lang = {
-  go = require("rei.lang.go")
+  go = require("rei.lang.go"),
+  ts = require("rei.lang.ts"),
 }
 
 return M
