e9a2d9fdec356a59fda9ff5478af39c684f05d69
wheat-thin-wiens <ethanjwiens@gmail.com>
2025-12-24 14:14:51 -0600

    add java

--- lua/rei/langs/init.lua
+++ lua/rei/langs/init.lua
@@ -4,8 +4,9 @@ P = require("rei.palette").defaults
 Transparency = require("rei.utils").transparency_enabled()
 
 M.langs = {
-  go = require("rei.langs.go"),
-  ts = require("rei.langs.ts"),
+  go   = require("rei.langs.go"),
+  java = require("rei.langs.java"),
+  ts   = require("rei.langs.ts"),
 }
 
 return M
---
+++ lua/rei/langs/java.lua
@@ -0,0 +1,5 @@
+local M = {
+  ["@lsp.type.modifier.java"] = { fg = P.blue },
+}
+
+return M
