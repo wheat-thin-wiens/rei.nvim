e551ac9d6b05c4f6a706108c66ccad39ae925bbe
wheat-thin-wiens <ethanjwiens@gmail.com>
2026-03-04 15:53:52 -0600

    wip java lang support

--- lua/rei/langs/java.lua
+++ lua/rei/langs/java.lua
@@ -1,11 +1,15 @@
 local M = {
   ["@type.java"] = { fg = P.blue },
 
+  ["@lsp.type.builtin.java"] = { fg = P.yellow },
   ["@lsp.type.class.java"] = { fg = P.blue },
-  ["@lsp.type.modifier.java"] = { fg = P.bright_red },
+  ["@lsp.type.modifier.java"] = { fg = P.yellow },
+  ["@lsp.type.namespace.java"] = { fg = P.fg, italic = false },
 
   ["@lsp.typemod.property.declaration.java"] = { fg = P.purple },
   ["@lsp.typemod.property.private.java"] = { fg = P.purple },
+
+  ["@variable.builtin.java"] = { fg = P.fg },
 }
 
 return M
