1eb364f2234b55804b9ea412a8b5bad3cbdaf648
wheat-thin-wiens <ethanjwiens@gmail.com>
2026-06-15 11:53:34 -0500

    chore: fix palette formatting

--- lua/rei/palette.lua
+++ lua/rei/palette.lua
@@ -1,24 +1,24 @@
 local M = {}
 
 M.defaults = {
-  bg = "#0E1723",
-  fg = "#CDCDCD",
+  bg             = "#0E1723",
+  fg             = "#CDCDCD",
 
-  subtext1 = "#9F9F9F",
-  subtext2 = "#868686",
-  subtext3 = "#717171",
-  subtext4 = "#5F5F5F",
+  subtext1       = "#9F9F9F",
+  subtext2       = "#868686",
+  subtext3       = "#717171",
+  subtext4       = "#5F5F5F",
 
-  bg_dark = "#0C131D", -- #101B29
-  black   = "#556173",
-  red     = "#A87692",
-  green   = "#809FA1",
-  orange  = "#FD9491",
-  blue    = "#91B8E9",
-  purple  = "#B9AEE6",
-  yellow  = "#FCD0A1",
-  cyan    = "#9DBCD0",
-  white   = "#DAD0E9",
+  bg_dark        = "#0C131D", -- #101B29
+  black          = "#556173",
+  red            = "#A87692",
+  green          = "#809FA1",
+  orange         = "#FD9491",
+  blue           = "#91B8E9",
+  purple         = "#B9AEE6",
+  yellow         = "#FCD0A1",
+  cyan           = "#9DBCD0",
+  white          = "#DAD0E9",
 
   bright_black   = "#7C90B8",
   bright_red     = "#D9B0C0",
@@ -30,23 +30,23 @@ M.defaults = {
   bright_cyan    = "#A5C1D4",
   bright_white   = "#DDD4EB",
 
-  dark_black   = "#444E5C",
-  dark_red     = "#2D2027",
-  dark_green   = "#364344",
-  dark_orange  = "#683D3B",
-  dark_blue    = "#3C4D60",
-  dark_purple  = "#48445A",
-  dark_yellow  = "#7A654E",
-  dark_cyan    = "#4C5C65",
-  dark_white   = "#938D9E",
+  dark_black     = "#444E5C",
+  dark_red       = "#2D2027",
+  dark_green     = "#364344",
+  dark_orange    = "#683D3B",
+  dark_blue      = "#3C4D60",
+  dark_purple    = "#48445A",
+  dark_yellow    = "#7A654E",
+  dark_cyan      = "#4C5C65",
+  dark_white     = "#938D9E",
 
-  grey0 = "#20262E",
-  grey1 = "#2F3843",
-  grey2 = "#404A56",
-  grey3 = "#49515E",
-  grey4 = "#575F6B",
+  grey0          = "#20262E",
+  grey1          = "#2F3843",
+  grey2          = "#404A56",
+  grey3          = "#49515E",
+  grey4          = "#575F6B",
 
-  none = "NONE"
+  none           = "NONE"
 }
 
 return M
