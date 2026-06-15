4b5ee7e12509de24a302cde998936da1f50c738b
wheat-thin-wiens <ethanjwiens@gmail.com>
2026-06-15 16:13:29 -0500

    feat(hrsh7th_cmp): added highlights

--- lua/rei/plugins/hrsh7th_cmp.lua
+++ lua/rei/plugins/hrsh7th_cmp.lua
@@ -1,15 +1,21 @@
+-- local syntax = require("rei.builtins.syntax")
 local italic = require("rei.config").integrations.hrsh7th_cmp.italic_highlight
 
 return {
-  Pmenu               = { bg = Transparency or P.bg },
-  PmenuSel            = { fg = P.white, bg = P.dark_black, italic = italic },
+  Pmenu                   = { bg = Transparency or P.bg },
+  PmenuSel                = { fg = P.white, bg = P.dark_black, italic = italic },
 
-  CmpItemDefault      = { fg = P.blue, bg = Transparency or P.bg },
-  CmpItemKindVariable = { fg = P.fg },
-  CmpItemKindFunction = { fg = P.purple },
-  CmpItemKindText     = { fg = P.green },
-  CmpItemKindSnippet  = { fg = P.orange },
-  CmpItemKindField    = { fg = P.purple },
-  CmpItemKindProperty = { fg = P.purple },
-  CmpItemKindEnum     = { fg = P.red },
+  CmpItemDefault          = { fg = P.blue, bg = Transparency or P.bg },
+  CmpItemKind             = { fg = P.blue },
+  CmpItemKindVariable     = { fg = P.fg },
+  CmpItemKindFunction     = { link = "Function" },
+  CmpItemKindText         = { link = "String" },
+  CmpItemKindSnippet      = { fg = P.orange },
+  CmpItemKindField        = { fg = P.purple },
+  CmpItemKindProperty     = { fg = P.purple },
+  CmpItemKindEnum         = { fg = P.red },
+
+  CmpItemAbbrMatchDefault = { fg = P.blue },
+  CmpItemAbbrMatch        = { fg = P.blue },
+  CmpItemAbbrMatchFuzzy   = { fg = P.yellow },
 }
